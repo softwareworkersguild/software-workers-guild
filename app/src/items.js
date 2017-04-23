b15ccb2086362b22aacb924ff9fbc12c6f7ec076
Akiva Leffert <aleffert@gmail.com>
2017-04-23 12:45:46 -0400

    Add stub item list

---
+++ app/src/items.js
@@ -0,0 +1,10 @@
+[
+    {
+        "id": "public-diversity-stats",
+        "text": "Publish diversity statistics"
+    },
+    {
+        "id": "reasonable-on-call",
+        "text": "Reasonable on call policies (see link for what reasonable means...)"
+    }
+]
